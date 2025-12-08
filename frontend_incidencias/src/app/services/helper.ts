084651dda7d2938ec9b6396adbfcd451a5272ca9
DESKTOP-HIVU6KK\Celia <marinalvarezcelia@gmail.com>
2025-12-08 18:08:28 +0100

    carpeta dist

--- frontend_incidencias/src/app/services/helper.ts
+++ frontend_incidencias/src/app/services/helper.ts
@@ -1,2 +1,2 @@
-let baseUrl = 'http://localhost:8080';
+let baseUrl = 'http://ec2-13-62-222-150.eu-north-1.compute.amazonaws.com:8080';
 export default baseUrl;//para poder usar esta variable en otros ficheros
