5fa42b9f36f3c786707e5f6fe399614f0c626e10
DESKTOP-HIVU6KK\Celia <marinalvarezcelia@gmail.com>
2025-12-19 09:27:47 +0100

    localhost cambiado

--- frontend_incidencias/src/app/services/helper.ts
+++ frontend_incidencias/src/app/services/helper.ts
@@ -1,2 +1,2 @@
-let baseUrl = 'http://ec2-51-20-6-206.eu-north-1.compute.amazonaws.com:8080';
+let baseUrl = 'http://localhost:8443';
 export default baseUrl;//para poder usar esta variable en otros ficheros
