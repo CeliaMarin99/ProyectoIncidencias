90cee2a7f94237eb0c7df137f3f8a307ad2f5fdc
DESKTOP-HIVU6KK\Celia <marinalvarezcelia@gmail.com>
2025-12-19 07:55:44 +0100

    arreglando desppliegue

--- backend_incidencias/src/main/java/com/celia/backend/incidencias/backend_incidencias/BackendIncidenciasApplication.java
+++ backend_incidencias/src/main/java/com/celia/backend/incidencias/backend_incidencias/BackendIncidenciasApplication.java
@@ -2,10 +2,11 @@ package com.celia.backend.incidencias.backend_incidencias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class BackendIncidenciasApplication {
+public class BackendIncidenciasApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendIncidenciasApplication.class, args);
